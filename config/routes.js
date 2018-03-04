95f98e65bac16e46713b6418c5208f102dfe59dc
Eli Peters <eli@elipeters.com.au>
2018-03-04 17:37:24 +0930

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,19 @@
 module.exports.routes = {
 
   '/': { view: 'homepage' },
-  'get /login': { view: 'user/login' },
-  'get /signup': { view: 'user/signup' },
-  '/welcome': { view: 'user/welcome' },
+  
+   //User
+  'get /login': {view: 'user/login'},
+  'get /signup': {view: 'user/signup'},
+  
   'post /login': 'UserController.login',
   'post /signup': 'UserController.signup',
+  
+  'post /update': 'UserController.update',
   '/logout': 'UserController.logout',
+  '/welcome': {view: 'user/account'},
+  'get /edit': 'UserController.edit',
+  'get /account': 'UserController.account', 
+  'get /forgot': {view: 'user/recovery'},
+  'post /recovery': 'UserController.recovery', 
 };
