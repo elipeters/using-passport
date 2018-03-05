adc6de2fd75ac97c9910a94200b207ac18d07fc1
Eli Peters <eli@elipeters.com.au>
2018-03-05 12:59:28 +0930

    Update UserController.js

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -12,8 +12,8 @@ module.exports = {
 
   login: function (req, res) {           
       req.login(req.params.all(), function(err){
-        if(err) return res.redirect('back');
-        return res.redirect('/');
+        if(err) return res.view({msg: {type: "danger", body: "Incorrect username or password"}}, 'user/login');
+        return res.redirect('user/account');
       });   
   },
 
@@ -25,7 +25,7 @@ module.exports = {
   
 
   signup: function (req, res) {
-    return res.view('user/signup',{msg: {info: "All fields are required"}});
+    return res.view('user/signup',{msg: {type: "info", body: "All fields are required"}});
   },
   
   create: function (req, res) {
@@ -34,7 +34,7 @@ module.exports = {
     //check if account exists - need email for password recovery
     User.findOne({email: params.email}).exec(function(err,user){
       if(user){
-        return res.view('user/signup',{msg: {error: "That email is already in use"}});
+        return res.view('user/signup',{user: params, msg: {type: "danger", body: "That email is already in use"}});
       }else{
         //create user with hash password, we use the given password to login
         User.create({
@@ -63,11 +63,9 @@ module.exports = {
     res.view('user/account',{user: req.user});
   },
   
-  
   edit: function(req,res){ 
-    res.view('user/edit',{user: req.user, msg: {}});
+    res.view('user/edit',{user: req.user, msg: {type: "info", body: "All fields are required"}});
   },
-  
   
   update: function(req,res){ //TODO: hash password
     var params = req.params.all();    
@@ -100,7 +98,7 @@ module.exports = {
         req.user = user[0];
         
         //send successful user update
-        res.view('user/edit', {user: user[0],msg:{success:'Your details have been updated'}});
+        res.view('user/edit', {user: user[0],msg:{type: "success", body:'Your details have been updated'}});
       });      
       
     });                                        
@@ -117,7 +115,7 @@ module.exports = {
 
       //TODO: send recovery email
       
-      res.view('user/recovery',{msg: msg});
+      res.view('user/recovery',{msg: {type: "success", body: "Email has been sent."}});
     });
   },
 };
