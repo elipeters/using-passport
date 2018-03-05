02196810a839ef5bdcbf1a41ea31d7ae5a6e2653
Eli Peters <eli@elipeters.com.au>
2018-03-05 13:40:25 +0930

    Update policies.js

--- config/policies.js
+++ config/policies.js
@@ -18,42 +18,14 @@
 
 
 module.exports.policies = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions (`true` allows public     *
-  * access)                                                                  *
-  *                                                                          *
-  ***************************************************************************/
-
-  // '*': true,
-
-  /***************************************************************************
-  *                                                                          *
-  * Here's an example of mapping some policies to run before a controller    *
-  * and its actions                                                          *
-  *                                                                          *
-  ***************************************************************************/
+  
   UserController: {
     '*': 'isAuthenticated',
     login: true, // should always be accessible
     signup: true, 
     forgot: true,
-    recovery: true
-  }
+    recovery: true,
+    create: true
+  },
 
-	// RabbitController: {
-
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
-
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
-
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
-	// }
 };
